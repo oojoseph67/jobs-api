b52ae7dbdcf896fe1c2eb2c7108e098a8038be01
Joseph Okolo <oojoseph67@gmail.com>
2024-02-08 20:58:55 +0100

    register auth setup

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,14 +1,27 @@
-
+const User = require("../models/user");
+const { StatusCodes } = require("http-status-codes");
+const { hashedPassword } = require("../utils/hash");
 
 const register = async (req, res) => {
-    res.send('register user')
-}
+  try {
+    const { body } = req;
+    const { name, email, password } = body;
+
+    const hashed = await hashedPassword(password);
+
+    const user = await User.create({ name, email, password: hashed });
+
+    res.status(StatusCodes.CREATED).json({ user });
+  } catch (error) {
+    console.log("🚀 ~ register ~ error:", error);
+  }
+};
 
 const login = async (req, res) => {
-    res.send('login user')
-}
+  res.send("login user");
+};
 
 module.exports = {
-    register,
-    login,
-}
+  register,
+  login,
+};
